25c4e77c846db22d04c170802b38cf48f88f3e4a
Daniel <danielga@outlook.pt>
2021-01-24 20:06:26 +0000

    Updated to project generator v2
    Updated README and license

---
+++ premake5.lua
@@ -0,0 +1,16 @@
+PROJECT_GENERATOR_VERSION = 2
+
+newoption({
+	trigger = "gmcommon",
+	description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
+	value = "path to garrysmod_common directory"
+})
+
+include(assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
+	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory"))
+
+CreateWorkspace({name = "xconsole"})
+	CreateProject({serverside = true})
+		warnings("Default")
+		IncludeSDKCommon()
+		IncludeSDKTier0()
